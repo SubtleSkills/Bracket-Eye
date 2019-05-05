77d0092865294fa71e6fdc544b526689142b92fa
Raghav Chadha <raghavc32@gmail.com>
2019-05-05 10:14:03 +0530

    adding files

---
+++ app/src/main/java/com/example/ah19/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ah19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+        "apikey": "v4XpMUiUEaF1W7xwUXXZVMdWkIu-YjYEn2fKokxuehwM",
+        "iam_apikey_description": "Auto-generated for key d76d7bde-d88a-40ea-920d-f1e9fa80c8f5",
+        "iam_apikey_name": "wdp-writer",
+        "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Writer",
+        "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/549de85aa4a24bfbbc8a9a187f713dd9::serviceid:ServiceId-d927cb35-d3e4-4eeb-a555-b623104f12ee",
+        "url": "https://gateway.watsonplatform.net/visual-recognition/api",
+
+    */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
